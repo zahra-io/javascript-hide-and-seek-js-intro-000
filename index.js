eac76954a27a7359bd829b48bbba368085e539bc
Zahra Khan <zarssskhan@gmail.com>
2017-09-19 22:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function getFirstSelector(selector) {
   var firstSelector = document.querySelector(selector)
   return firstSelector;
 };
+
+function nestedTarget() {
+  return document.querySelectorAll(".target #nested")
+}
